68be274c8f178ef2deba2e3e3d7328a35241a60a
Mohamed <justmoe23@gmail.com>
2022-09-10 16:53:33 -0400

    add poppins font

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer () {
 return (
 <footer className="footer">
         <div className="footer-container">
-            <p className="text">Ⓒ 2022 Team Tehnovengers. All right reserved. Created with React</p>
+            <p className="text">Ⓒ 2022 Team Tehnovengers. All right reserved. Created with React.</p>
           
         </div>
       </footer>
