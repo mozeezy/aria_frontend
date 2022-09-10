1a8b4516cc92262c40daac6e60022f8952c6822e
Ruchika Gosain <sharma.ruchika34@gmail.com>
2022-09-10 16:38:05 -0400

    style the footer.css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-
+import "./Footer.css"
 export default function Footer () {
 
 
 return (
-  <footer id="footer">Footer</footer>
+<footer className="footer">
+        <div className="footer-container">
+            <p className="text">Ⓒ 2022 Team Tehnovengers. All right reserved. Created with React</p>
+          
+        </div>
+      </footer>
 )
 }
