ec237bcf9316253d80a1f9937b584cdeaa4cd77e
Mohamed <justmoe23@gmail.com>
2022-09-10 17:57:40 -0400

    before integrating google maps API

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Header from "./components/Header/Header";
 import Map from "./components/Map/Map";
 import Model from "./components/Model/Model";
 import Footer from "./components/Footer/Footer";
+
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Header />
-      < Map />
+      <Map />
       <Model />
       <Footer />
     </div>
